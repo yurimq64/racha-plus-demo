ceef8251c7ec8f3049943ca8e27a75d1aebc8ed3
yuri <ymendesqueiroz@gmail.com>
2025-12-04 19:35:26 -0300

    fix: corrige porta do localhost

--- backend/racha-plus-demo-api/src/main/java/com/rachaplusdemo/api/config/SecurityConfig.java
+++ backend/racha-plus-demo-api/src/main/java/com/rachaplusdemo/api/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
